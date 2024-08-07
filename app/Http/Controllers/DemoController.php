f6afeebab9e6eb1047e96550b01df5bb740ca748
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-08-07 15:38:54 +0600

    where-clause

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -8,7 +8,13 @@ use Illuminate\Http\Request;
 class DemoController extends Controller
 {
     public function DemoAction() {
-        // return Product::whereNull('price')->get();
-        return Product::whereNotNull('price')->get();
+        // return Product::whereDate('updated_at', '2024-08-03')->get();
+        // return Product::whereMonth('updated_at', '08')->get();
+        // return Product::whereDay('updated_at', '03')->get();
+        // return Product::whereYear('updated_at', '2024')->get();
+        // return Product::whereTime('created_at', '20:08:12')->get();
+        // return Product::whereTime('updated_at', '17:03:23')->get();
+        return Product::whereTime('created_at', '>', 'updated_at')->get();
+        // return Product::whereTime('created_at', '<', 'updated_at')->get();
     }
 }
