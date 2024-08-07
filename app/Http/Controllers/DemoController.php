95578451e371ece100b779bab9063dcc6c68a748
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-08-07 16:28:12 +0600

    group-by

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Brand;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
     public function DemoAction() {
-        // return Brand::latest()->first();
-        return Brand::oldest()->first();
+        return Product::groupBy('price')->get();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/latest-oldest', [DemoController::class, 'DemoAction']);
+Route::get('/group-by', [DemoController::class, 'DemoAction']);
