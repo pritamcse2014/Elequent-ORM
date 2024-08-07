efdd626b9485983dd44a30b8fa5672ec4adafa1c
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-08-07 16:22:27 +0600

    latest-oldest

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
-use App\Models\Product;
 use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
     public function DemoAction() {
-        // return Product::orderBy('price', 'desc')->get();
-        // return Product::orderBy('price', 'asc')->get();
-        return Brand::inRandomOrder()->first();
+        // return Brand::latest()->first();
+        return Brand::oldest()->first();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/order', [DemoController::class, 'DemoAction']);
+Route::get('/latest-oldest', [DemoController::class, 'DemoAction']);
